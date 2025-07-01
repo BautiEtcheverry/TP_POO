f2c010beb36325931e5e5c48a83607bff8f4b540
juanhumphreys <jhumphreys@itba.edu.ar>
2025-07-01 11:58:46 -0300

    pongo los getters en el front

--- src/backend/model/Figure.java
+++ src/backend/model/Figure.java
@@ -1,5 +1,11 @@
 package backend.model;
 
+import javafx.scene.paint.Color;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public interface Figure {
+    static Map<Figure, Color> figureColorMap = new HashMap<>;
 
 }
