7d0e320f5fb170161f9a466cf2f826a1d2012ffb
benicio <bleite@itba.edu.ar>
2025-07-01 11:48:23 -0300

    cambie cosas de las figuras

--- src/backend/model/Point.java
+++ src/backend/model/Point.java
@@ -1,13 +1,37 @@
 package backend.model;
 
+import java.util.Objects;
+
 public class Point {
 
-    public double x, y;
+    private final double x, y;
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Point point &&
+                Double.compare(x, point.x) == 0 &&
+                Double.compare(y, point.y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+
 
     @Override
     public String toString() {
